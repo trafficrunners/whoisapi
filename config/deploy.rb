be878eee4985b1a84e28cbb5f536999bd0976dec
Nate Fischer <nate@trafficrunners.net>
2015-06-03 14:48:22 -0700

    m

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,8 @@
 # config valid only for Capistrano 3.1
 lock '3.1.0'
 
+SSHKit.config.command_map[:rake] = "bundle exec rake"
+
 set :application, 'bulkwhois.io'
 set :repo_url, 'git@github.com:trafficrunners/whoisapi.git'
 
@@ -8,7 +10,7 @@ set :repo_url, 'git@github.com:trafficrunners/whoisapi.git'
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
 # Default deploy_to directory is /var/www/my_app
-# set :deploy_to, '/var/www/my_app'
+set :deploy_to, '/home/deployer/apps/bulkwhois'
 
 # Default value for :scm is :git
 # set :scm, :git
