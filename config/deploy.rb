568e8d36ad3c9f1c255dac9d1b3236bfa390f986
Jimmy Qian <jimmyqian@gmail.com>
2015-06-03 16:34:23 -0700

    increase whois count

--- config/deploy.rb
+++ config/deploy.rb
@@ -41,7 +41,7 @@ namespace :deploy do
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      invoke 'unicorn:reload'
+      invoke 'unicorn:restart'
       # Your restart mechanism here, for example:
       # execute :touch, release_path.join('tmp/restart.txt')
     end
